bcb1e37739b844dd49103eed5c787dfae5e487a3
santhosh-jagadeesan <santhoshjagadeesan93@gmail.com>
2023-12-05 20:14:49 +0530

    ...selenium test added

---
+++ src/test/java/com/santhoshjagadeesan/SeleniumLoginTest.java
@@ -0,0 +1,33 @@
+package com.santhoshjagadeesan;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SeleniumLoginTest {
+	
+	@Test
+	public void loginTest() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		
+		driver.findElement(By.name("username")).sendKeys("Admin");
+		driver.findElement(By.name("password")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[contains(@class,'orangehrm-login-button')]")).click();
+		
+		Assert.assertTrue(driver.getCurrentUrl().endsWith("dashboard/index"));
+		
+		driver.quit();
+	}
+
+}
